6c534f9ed2643229e36b04bf45f34ec1fa2b4239
Emily G <aspentree79@gmail.com>
2019-07-25 18:22:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,6 +10,7 @@
   end
 
 
+
   def reduce(source_array, starting_point=0)
       i = 0
       while i < source_array.count
@@ -18,32 +19,3 @@
       end
       return starting_point
     end
-
-
-    it "returns false when any value is false" do
-      source_array = [1, 2, true, "razmatazz", false]
-      expect(reduce(source_array){|memo, n| memo && n}).to be_falsy
-    end
-
-    it "returns true when a truthy value is present" do
-      source_array = [ false, nil, nil, nil, true]
-      expect(reduce(source_array){|memo, n| memo || n}).to eq(true)
-    end
-
-
-def reduce(source_array)
-if source_array.any?(false)
-    return false
-  else
-  return true
-  end
-end
-
-
-def reduce(source_array)
-if source_array.any?(true)
-  return true
-else
-  return false
-end
-end
