3d32dbbbf638486e2d7602359953ed9cad5a1518
Emily G <aspentree79@gmail.com>
2019-07-25 18:31:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,12 @@
   end
 
 
-  def reduce(source_array, starting_point=0)
+  def reduce(source_array, starting_point=nil)
       i = 0
+      if starting_point
+        accumulator = starting_point
+      else
+        accumulator = source_array[0]
       while i < source_array.count
   yield starting_point += source_array[i]
         i += 1
