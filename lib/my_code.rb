72733867b588b6e6724a2995b0a67f42fecfc8b4
Emily G <aspentree79@gmail.com>
2019-07-25 17:55:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,38 @@
-# Your Code Here
+
+  def map(array)
+  negatives = []
+  i = 0
+    while i < array.length
+    negatives.push(yield (array[i]))
+    i += 1
+    end
+    negatives
+  end
+
+
+  def reduce(source_array, starting_point=0)
+      i = 0
+      while i < source_array.count
+  yield starting_point += source_array[i]
+        i += 1
+      end
+      return starting_point
+    end
+
+
+def reduce(source_array)
+if source_array.any?(false)
+    return false
+  else
+  return true
+  end
+end
+
+
+def reduce(source_array)
+if source_array.any?(true)
+  return true
+else
+  return false
+end
+end
