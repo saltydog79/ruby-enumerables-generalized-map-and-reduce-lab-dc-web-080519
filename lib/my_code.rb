12c8c95b19eec92ace7c3ba2f9de42cc5d111408
Emily G <aspentree79@gmail.com>
2019-07-25 18:50:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+require 'pry'
+  def map(array)
+  negatives = []
+  i = 0
+    while i < array.length
+    negatives.push(yield (array[i]))
+    i += 1
+    end
+    negatives
+  end
+
+
+  def reduce(source_array, starting_point=nil)
+
+      if starting_point
+        accumulator = starting_point
+        i = 0
+      else
+        accumulator = source_array[0]
+        i = 1
+      end
+      while i < source_array.count
+accumulator =  yield(accumulator, source_array[i])
+# binding.pry
+        i += 1
+      end
+      return accumulator
+    end
